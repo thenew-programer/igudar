5ebdc3400036f662f87fad8b750fb0de852e71c9
Youssef Bouryal <youssefbouryal02@gmail.com>
2025-06-20 00:35:36 +0100

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,10 +57,10 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={inter.variable}>
 			<head>
-				<link rel="icon" href="/favicon.ico" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="icon" href="/favicon.jpeg" />
+				<link rel="apple-touch-icon" sizes="180x180" href="/favicon.jpeg" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon.jpeg" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon.jpeg" />
 				{/*<link rel="manifest" href="/site.webmanifest" />*/}
 				<meta name="theme-color" content="#5d18e9" />
 				<meta name="msapplication-TileColor" content="#5d18e9" />
